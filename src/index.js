f6f0b04872d726fc2ecb3a77fbc26e10d5ceec96
[kuba] <[poczta@kubaadamczyk]>
2022-02-13 15:44:42 +0100

    conect with Michal's server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, InMemoryCache, HttpLink, gql, ApolloProvider } from '@apollo/client';
 import App from './App';
+
+const Url = "https://examples.devmastery.pl/random-stuff/graphql";
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: Url
+  })
+});
+
+const randomQuote = gql`
+  query getQuote {
+    randomQuote {
+      text
+      author
+    }
+  }
+`;
+
+client.query({query: randomQuote}).then(result => console.log(result.data));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>    
   </React.StrictMode>,
   document.getElementById('root')
 );
